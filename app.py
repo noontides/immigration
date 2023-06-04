388c40a6c95b72ef3e78bfbcbb3fd59b5da44930
Voke Elstein <42821434+noontides@users.noreply.github.com>
2023-06-03 22:16:56 -0700

    Update app.py

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ if query:
     #llm = OpenAI(temperature=0, openai_api_key=OPENAI_API_KEY, model_name="gpt-3.5-turbo")
     llm = OpenAI(temperature=0, api_key=OPENAI_API_KEY, model_name="gpt-3.5-turbo")
     memory = ConversationBufferMemory()
-    conversation = ConversationChain(llm=llm, verbose=True, memory=memory)
+    conversation = ConversationChain(llm=llm, memory=memory)
     from langchain.chains.question_answering import load_qa_chain
     chain = load_qa_chain(llm, chain_type="stuff")
 
