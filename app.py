d610af2c38dc8f54c235df59cdbe5af3e87c8582
Voke Elstein <42821434+noontides@users.noreply.github.com>
2023-06-03 22:12:58 -0700

    Update app.py

--- app.py
+++ app.py
@@ -91,7 +91,8 @@ if query:
     # Generate prompt with updated conversation history
     prompt = template.format(conversation_text=conversation_text)
 
-    llm = OpenAI(temperature=0, openai_api_key=OPENAI_API_KEY, model_name="gpt-3.5-turbo")
+    #llm = OpenAI(temperature=0, openai_api_key=OPENAI_API_KEY, model_name="gpt-3.5-turbo")
+    llm = OpenAI(temperature=0, api_key=OPENAI_API_KEY, model_name="gpt-3.5-turbo")
     memory = ConversationBufferMemory()
     conversation = ConversationChain(llm=llm, verbose=True, memory=memory)
     from langchain.chains.question_answering import load_qa_chain
